0c486518bb40a819c227150371f3d2e322859256
Ugochukwu <dykeehenry@gmail.com>
2021-01-24 20:45:55 +0100

    design container

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -58,6 +58,16 @@ class _HomePageState extends State<HomePage> {
                           return Padding(
                             padding: const EdgeInsets.all(5.0),
                             child: Container(
+                              decoration: BoxDecoration(
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Colors.grey.withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3), // changes position of shadow
+                                  ),
+                                ],
+                              ),
                               child: Padding(
                                 padding: const EdgeInsets.all(10.0),
                                 child: Text(
