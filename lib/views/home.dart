87039ccc9592e0a2ff46d1e89f5ace7a0c90592d
Ugochukwu <dykeehenry@gmail.com>
2021-01-25 13:31:24 +0100

    updated

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -61,10 +61,9 @@ class _HomePageState extends State<HomePage> {
                               decoration: BoxDecoration(
                                 boxShadow: [
                                   BoxShadow(
-                                    color: Colors.grey.withOpacity(0.5),
-                                    spreadRadius: 5,
-                                    blurRadius: 7,
-                                    offset: Offset(0, 3), // changes position of shadow
+                                    color: Colors.grey.withOpacity(0.1),
+                                    spreadRadius: 1,
+                                    offset: Offset(0, 1), // changes position of shadow
                                   ),
                                 ],
                               ),
